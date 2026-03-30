de9142d06e54ecae682cc8b8f468994a7238e3a3
Faith Kamande <faithkamande029@gmail.com>
2026-03-30 08:43:17 +0300

    Budget calculator function

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+// function representing monetary value
+function calculateTax(amount) {
+    let tax = 0.10;
+    let totalTaxed = amount * tax;
+    return totalTaxed;
+}
 
+// function converts string into upperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+// function to return larger number of two
+function findMaximum(num1, num2){
+    if (num1 > num2){
+        return num1
+    }else {
+        return num2
+    }
+}
 
+// function returns true if string is palidrome
+function isPalindrome(word) {
+    let letters = word.split("");
+
+    let reversedLetters = letters.reverse();
+    let reverseWord = reversedLetters.join("");
+
+    if (word === reverseWord) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+// function calculates and returns price after discount
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let totalDiscount = originalPrice * (discountPercentage / 100);
+    let totalPrice = originalPrice - totalDiscount;
+
+    return totalPrice;
+}
 
 
 // This is required for the test to function properly  
